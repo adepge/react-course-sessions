2580f592c2b67ee39915288e47ebd03a6549e000
Ed <edward.blewitt2@gmail.com>
2024-02-02 15:38:11 +0000

    Week 1

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import styles from "./App.module.css"
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className={styles.header} >Movie app</h1>
+      <p>Welcome to my app!</p>
+      <hr />
+    </>
+  )
+}
+
+export default App
